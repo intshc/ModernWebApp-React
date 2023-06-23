c37fbc7355c1adede9412cfcc67bb0950a3ba08f
Hyeongchan Shin <intshc@gmail.com>
2023-06-23 16:28:09 +0900

    날씨 API로 화면 띄우기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  
-  return (
-    <div className="App">
+  const [temp, setTemp] = useState("");
+  const [desc, setDesc] = useState("");
+  const [icon, setIcon] = useState("");
+  const [isReady, setReady] = useState("");
 
-    </div>
-  );
-};
+  const apikey= process.env.WEATHER_APIKEY;
+  React.useEffect(()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Seoul&APPID=${apikey}&units=metric`)
+    .then(result => result.json())
+    .then(jsonresult =>{
+      setTemp(jsonresult.main.temp);
+      setDesc(jsonresult.weather[0].main);
+      setIcon(jsonresult.weather[0].icon);
+      setReady(true);
+    })
+    .catch(err => console.error(err))
+  }, [])
+
+  if(isReady){
+    return (
+      <div className="App">
+          <p>온도: {temp}</p>
+          <p>상태: {desc}</p>
+          <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+            alt="Weather icon"></img>
+      </div>
+    );
+  }
+  else{
+    return(
+      <div>Loading...</div>
+    )
+  }
+}
 
 export default App;

5b014843582ecef93d9930b0e507820c10acba7a
Hyeongchan Shin <intshc@gmail.com>
2023-06-25 23:25:54 +0900

    정렬, 필터, 페이징 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React, {useState} from "react";
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 
 function App() {
 
@@ -14,24 +17,31 @@ function App() {
             .catch(err => console.error(err))
   }
 
+  const columns = [
+    {field: 'full_name', sortable: true, filter: true},
+    {field: 'html_url', sortable: true, filter: true},
+    {field: 'owner.login', sortable: true, filter: true},
+    {
+      field: 'full_name',
+      cellRenderer: params =>
+              <button
+                      onClick={() => alert(params.value)}>
+                Press me
+              </button>
+    }
+  ]
   return (
           <div className="App">
-            <input value={keyword} onChange={e => setKeyword(e.target.value)} />
+            <input value={keyword} onChange={e => setKeyword(e.target.value)}/>
             <button onClick={fetchData}>Fetch</button>
-            <table>
-              <tbody>
-              {
-                data.map(repo =>
-                <tr key={repo.id}>
-                  <td>{repo.full_name}</td>
-                  <td>
-                    <a href={repo.html_url}>{repo.html_url}</a>
-                  </td>
-                </tr>
-                )
-              }
-              </tbody>
-            </table>
+            <div className="ag-theme-material" style={{height: 500, width: '90%'}}>
+              <AgGridReact
+                      rowData={data}
+                      columnDefs={columns}
+                      pagination={true}
+                      paginationPageSize={8}
+              />
+            </div>
           </div>
   );
 }
